925c1b0860185553c1a590ea93da8b4b32c7ba48
Connor <xiaobang199806@163.com>
2023-03-25 19:54:42 +0800

    update parking

--- 01_车场计费策略.py
+++ 01_车场计费策略.py
@@ -3,73 +3,97 @@
 # 晚10点到早8点：15分钟免费，每小时2元，夜间封顶5元。
 # 根据入场出场时间，输出计费金额
 
-# 导入 datetime 模块
+# 导入 datetime和math模块
 import datetime
+import math
 # 定义费用和时间
+fees = 0  # 定义总停车费
 top_cost = 5  # 夜间封顶费用
 per_hours_cost = 2  # 每小时停车费
-fees = 0  # 定义总停车费
 day_hours = 14  # 定义无封顶费用的停车时间，22-8总共14小时
 
-if True:
-    # 模拟汽车入场，实际场景是接收从地磁传来的信号？
-    admission = bool(input('车辆是否入场（输入任意字符代表入场）：'))
+# 模拟汽车入场，实际场景是接收从地磁传来的信号？
+admission = bool(input('车辆是否入场（输入任意字符代表入场）：'))
+if admission:
     print('入场成功')
-
     # 获取车辆入场和出场时间
-    in_time = datetime.datetime.now()  # 获取当前时间
-    time_str = input('请输入汽车的出场时间（格式：年-月-日 小时:分钟:秒）：')  # 手动输入汽车时间，实际场景应该是汽车出场传回出场时间
-    out_time = datetime.datetime.strptime(time_str, "%Y-%m-%d %H:%M:%S")  # 将输入的字符串解析为日期和时间
+    time_str1 = input('请输入车辆入场时间（格式：年-月-日 小时:分钟:秒）：')   # 手动输入车辆入场时间
+    entry_time = datetime.datetime.strptime(time_str1, "%Y-%m-%d %H:%M:%S")  # 将输入的字符串解析为日期和时间
+    time_str2 = input('请输入出场时间（格式：年-月-日 小时:分钟:秒）：')  # 手动输入车辆出场时间
+    exit_time = datetime.datetime.strptime(time_str2, "%Y-%m-%d %H:%M:%S")
+    print(f'入场时间：{entry_time}')
+    print(f'出场时间：{exit_time}')
 
-    # 定义一个临界时间，该时间为车辆入场当天的早上6点
+    # 获取车辆的入场日期和出场日期之差
+    entry_date = entry_time.date()  # 入场日期
+    exit_date = exit_time.date()  # 出场日期
+    date_diff = max((exit_date - entry_date).days, 0)
+
+    # 定义一个入场临界时间，该时间为车辆入场当天的早上6点
     # 因为在6点之后停车不可能触发夜间封顶费用
     # 而在当天6点之前停车就有可能触发夜间封顶费用
-    critical_time = datetime.datetime.combine(in_time.date(), datetime.time(hour=6, minute=0, second=0))
+    critical_time = datetime.datetime.combine(entry_time.date(), datetime.time(hour=6, minute=0, second=0))
+
+    # 定义一个入场时间差，用于计算车辆在22:00之前入场时，而产生的停车时间
+    # 定义晚上10点，用于计算差值
+    night_time = datetime.datetime.combine(entry_time.date(), datetime.time(hour=22, minute=0, second=0))
+    # 计算时间差
+    entry_diff = (night_time - entry_time).total_seconds() // 3600
+    entry_diff = max(0, entry_diff)  # 如果时间差为负数则视为0
+    entry_diff = math.ceil(entry_diff)  # 向上取整
+
+    # 同上，定义一个出场时间差，用于计算车辆出场当天，晚于8点出场而产生的停车时间
+    morning_time = datetime.datetime.combine(exit_time.date(), datetime.time(hour=8, minute=0, second=0))
+    # 计算时间差，步骤合并
+    exit_diff = math.ceil(max(0, (exit_time - morning_time).total_seconds() // 3600))
 
     # 计算停车时间
-    parking_time = out_time - in_time
-
+    parking_time = exit_time - entry_time
     # 分别获取停车时间的天数，秒数和微秒数
     days = parking_time.days
     seconds = parking_time.seconds
     microseconds = parking_time.microseconds
-
     # 将秒和微秒数合并为总秒数
     total_seconds = seconds + microseconds / 1000000
-
     # 计算小时、分钟和秒数
     hours, remainder = divmod(total_seconds, 3600)
     minutes, seconds = divmod(remainder, 60)
 
-    # 判断停车时间是否超过15min，不超即免费
-    if parking_time < datetime.timedelta(minutes=15):  # 判断停车时间是否超过15min
+    # 判断停车时间是否超过15min，不超过即免费
+    if parking_time <= datetime.timedelta(minutes=15):  # 判断停车时间是否超过15min
         fees = 0
         print(f'您的停车时间为{days}天{hours}时{minutes}分{seconds}秒，停车费为{fees}元')
-    else:
-        # 判断停车时间是否小于2小时
-        if parking_time < datetime.timedelta(hours=2):
-            # 不超过2小时就按每小时2元计算，因为向上取整所以hours要+1
-            if minutes > 0:
-                fees = (hours + 1) * per_hours_cost
-                print(f'您的停车时间为{days}天{hours}时{minutes}分{seconds}秒，停车费为{fees}元')
-            # 排除刚好2小时0分的情况，防止多计算一小时费用
-            else:
-                fees = hours * per_hours_cost
-                print(f'您的停车时间为{days}天{hours}时{minutes}分{seconds}秒，停车费为{fees}元')
-        else:
-            # 超过两小时后就判断停车时间是否在早上6点之后
-            if in_time > critical_time:
-                # 判断从车辆入场到出场是否跨越了一天
-                if
 
+    # 判断停车时间是否超过2小时
+    elif parking_time <= datetime.timedelta(hours=2):
+        # 判断是否为整小时，例如1小时0分0秒
+        if seconds > 0:
+            # 非整小时：费用中的小时数向上取整
+            fees = (hours + 1) * per_hours_cost
+            print(f'您的停车时间为{days}天{hours}时{minutes}分{seconds}秒，停车费为{fees}元')
+        else:
+            # 整小时：小时数不用向上取整
+            fees = hours * per_hours_cost
+            print(f'您的停车时间为{days}天{hours}时{minutes}分{seconds}秒，停车费为{fees}元')
+
+    # 判断停车时间是否在早上6点之前
+    elif entry_time < critical_time:
+        # 计算夜间封顶费用
+        fees += top_cost * (date_diff + 1)
+        # 停车费公式
+        fees += ((day_hours * date_diff) + exit_diff) * per_hours_cost
+        print(f'您的停车时间为{days}天{hours}时{minutes}分{seconds}秒，停车费为{fees}元')
+
+    else:
+        # 计算夜间封顶费用
+        fees += top_cost * date_diff
+        # 停车费公式
+        fees += (entry_diff + exit_diff + day_hours * (date_diff - 1)) * per_hours_cost
+        print(f'您的停车时间为{days}天{hours}时{minutes}分{seconds}秒，停车费为{fees}元')
 
 else:
-    print('暂无汽车入场')
-# car_in = time.localtime()
-# car_out = time.localtime() 15min / 10:00 - 20:00  / 20:00 - 9:59 / 1day
+    print('入场失败或者暂无汽车入场')
 
 
-print(type(in_time))
-print(in_time)
-print(type(out_time))
-print(out_time)
+
+
--- 02_datetime_test.py
+++ 02_datetime_test.py
@@ -1,21 +1,20 @@
 # import datetime
 #
-# # 获取当前时间
-# in_time = datetime.datetime.now()
+# 获取当前时间
+import datetime
+
+in_time = datetime.datetime.now()
+
+time_str = input('请输入汽车的出场时间（格式：年-月-日 小时:分钟:秒）：')  #
+out_time = datetime.datetime.strptime(time_str, "%Y-%m-%d %H:%M:%S")
+
 #
-# # 从控制台读取时间，格式为“小时:分钟:秒”
-# time_str = input("请输入时间（格式：小时:分钟:秒）：")
-# hours, minutes, seconds = map(int, time_str.split(':'))
-#
-# # 创建一个timedelta对象来表示输入的时间
-# out_time = datetime.timedelta(hours=hours, minutes=minutes, seconds=seconds)
-#
-# # 计算out_time + in_time
-# result_time = in_time + out_time
-#
-# # 输出结果
-# print("输入的时间为：", out_time)
-# print("当前时间为：", in_time)
-# print("结果为：", result_time)
+# 计算out_time - in_time
+result_time = datetime.date(out_time) - datetime.date(in_time)
+
+# 输出结果
+print("输入的时间为：", out_time)
+print("当前时间为：", in_time)
+print("结果为：", result_time)
 
 
